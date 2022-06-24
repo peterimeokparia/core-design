339ff55d1efc0e66347d563cf07bb912f7568765
peterimeokparia <68337147+peterimeokparia@users.noreply.github.com>
2022-06-24 15:41:08 -0400

    new repo test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// test git push on new repo.
+// test git push on new repo test.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
